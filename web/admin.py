2aadf971fb0b93c77568dc75a2d7df9c5790c72c
Enric Mieza <emieza@xtec.cat>
2014-12-12 14:23:26 +0100

    comments and doc

--- web/admin.py
+++ web/admin.py
@@ -2,29 +2,31 @@ from django.contrib import admin
 
 from models import *
 
+# ChannelAdmin requirements can be checked in issue #12 (create channel view)
+# https://github.com/lacetans/faceschool/issues/12
 class ChannelAdmin(admin.ModelAdmin):
     list_display = ('topic','responsible',)
     filter_horizontal = ('users',)
     def get_readonly_fields(self, request, obj=None):
-        # only superuser can edit channel responsible
         if request.user.is_superuser:
             return self.readonly_fields
-        # if not superuser, responsible will always be itself
+        # if not superuser, disable responsible field
         return self.readonly_fields + ('responsible',)
     def save_model(self, request, obj, form, change):
-        # superuser can modify responsible
+        # superuser can modify channel responsible
         if request.user.is_superuser:
             obj.save
             return
-        # normal users cannot modify channel owner (only themselves)
+        # force responsible to logged user (when creating channel)
         fsuser = request.user.fsuser
         obj.responsible = fsuser
         obj.save()
-    # admin list
     def queryset(self, request):
-        qs = super(ChannelAdmin, self).queryset(request)
+        qs = super(ChannelAdmin, self).get_queryset(request)
+        # superuser can see all channels
         if request.user.is_superuser:
             return qs
+        # show only owned channels (teachers)
         return qs.filter(responsible=request.user.fsuser)
         
 
