074b1fab690cf09ee7d08c8c8fc22bc3ad0949d8
Joaquin Montoliu <jmontoliu7@gmail.com>
2015-01-09 17:53:29 +0100

    usar comanda python manage.py load_data, aquesta carregara la llista csv que es diu usuaris.csv en web/management/command/

--- web/management/commands/load_data.py
+++ web/management/commands/load_data.py
@@ -3,15 +3,18 @@ from models import FSUser
 import csv
 import os.path
 import sys
+from django.db import models
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-csv_filepathname = PROJECT_ROOT+"/web/usuaris.ods"
+sys.path.append(PROJECT_ROOT)
+#os.environ['DJANGO_SETTINGS_MODULE'] = 'faceschool.settings'
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+csv_filepathname = PROJECT_ROOT+"/web/usuaris.csv"
 
 dataReader = csv.reader(open(csv_filepathname), delimiter=',', quotechar='"')
 for row in dataReader:
-    if row[0]!="username":
+    if row[0] != "username":
         FSUser = FSUser()
         FSUser.user.username = row[0]
         FSUser.user.first_name = row[1]
