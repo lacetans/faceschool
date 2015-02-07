3eaecf9f3cd6df1c5a8c4fb7cb630071df77924d
Joaquin Montoliu <jmontoliu7@gmail.com>
2015-02-07 12:35:09 +0100

    Ara ja es flexible segons el ordre de les columnes, i tambe controla que els cmaps de la capçalera sigui el estandar, no es poden inventar els camps. Solament queda veure com faig la relacio amb el canal on el alumne esta inscrit, suposo que sera amb la relacio FSUser.channel

--- web/management/commands/load_data.py
+++ web/management/commands/load_data.py
@@ -1,23 +1,67 @@
 __author__ = 'joaquin'
-from models import FSUser
+from web.models import FSUser
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
 import csv
 import os.path
 import sys
-from django.db import models
-
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-sys.path.append(PROJECT_ROOT)
-#os.environ['DJANGO_SETTINGS_MODULE'] = 'faceschool.settings'
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        csv_filepathname = PROJECT_ROOT+"/usuaris.csv"
+        sys.path.append(PROJECT_ROOT)
+        dataReader = csv.reader(open(csv_filepathname), delimiter=',', quotechar='"')
+        i = 0
+        index = {}
+        for row in dataReader:
+            if i==0:
+                index = self.comprovaCamps(row)
+            else:
+                usuari = User()
+
+                usuari.email = row[index.get('E-mail')]
+                usuari.username = row[index.get('Name')]
+                usuari.first_name = row[index.get('Name')]
+                usuari.last_name = row[index.get('Surname')]
+                usuari.save()
+
+            i += 1
+    def comprovaCamps(self, row):
+
+        row = ';'.join(row)
+
+        email = row.find("E-mail")
+        classroom = row.find("Classroom")
+        surname = row.find("Surname")
+        name = row.find("Name")
 
 
-csv_filepathname = PROJECT_ROOT+"/web/usuaris.csv"
+        if(email <= -1):
+            print """Error: E-mail not found """
+            return 0
+        elif(classroom <= -1):
+            print """Error: Classroom not found """
+            return 0
+        elif(name <= -1):
+            print """Error: Name not found"""
+            return 0
+        elif(surname <= -1 ):
+            print """Error: Surname and Name not found """
+            return 0
 
-dataReader = csv.reader(open(csv_filepathname), delimiter=',', quotechar='"')
-for row in dataReader:
-    if row[0] != "username":
-        FSUser = FSUser()
-        FSUser.user.username = row[0]
-        FSUser.user.first_name = row[1]
-        FSUser.user.last_name = row[2]
-        FSUser.user.email = row[3]
-        FSUser.save()
+        else:
+            row = row.split(";")
+            print row
+            index = {}
+            i = 0
+            for i in range(len(row)):
+                index[row[i]] = i
+            print index
+
+        return index
+
+
+
